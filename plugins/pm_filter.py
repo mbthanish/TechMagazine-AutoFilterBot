03d6eed46f1eead1be2bf9060941935d7ce69c04
mbthanish <75365521+mbthanish@users.noreply.github.com>
2024-04-02 22:18:11 +0530

    Update pm_filter.py

--- plugins/pm_filter.py
+++ plugins/pm_filter.py
@@ -670,14 +670,13 @@ async def auto_filter(client, msg, spoll=False):
     btn.insert(0, [
         InlineKeyboardButton(f'🎬 {search} 🎬', 'rkbtn')
     ])
-    btn.insert(1, [[
+    btn.insert(1, [
         InlineKeyboardButton("📺 🄼🄰🄸🄽 🄲🄷🄰🄽🄽🄴🄻 📺", url=f"https://t.me/adholokam_cinema_channel")
-    ],
-                   [
-        InlineKeyboardButton('📽 𝐎𝐓𝐓 𝐌𝐎𝐕𝐈𝐄𝐒 📽', 'https://t.me/+DFXw1-2my71hNTc1'),
-        Inlinekeyboardbutton('🖥 𝐎𝐓𝐓 𝐔𝐏𝐃𝐀𝐓𝐄𝐒 🖥', 'https://t.me/+1Zm5sYJIUpwyZWFl')
-    ]])
-        
+    ])
+    btn.insert(1, [
+        InlineKeyboardButton("📽️ 𝐎𝐓𝐓 𝐌𝐎𝐕𝐈𝐄𝐒 📽️", url=f"https://t.me/+DFXw1-2my71hNTc1"),
+        InlineKeyboardButton("🖥️ 𝐎𝐓𝐓 𝐔𝐏𝐃𝐀𝐓𝐄𝐒 🖥️", url=f"https://t.me/+1Zm5sYJIUpwyZWFl")
+    ])
     if offset != "":
         key = f"{message.chat.id}-{message.id}"
         BUTTONS[key] = search
